12f4f5809f625dff96edfc96bcb7ccc0144e045e
Lê Văn Dũng <dunghello36dz@gmail.com>
2026-03-29 13:31:13 +0700

    fix-be: xoa thua

--- Backend/src/main/java/docbooking/utils/Security.java
+++ Backend/src/main/java/docbooking/utils/Security.java
@@ -13,9 +13,4 @@ public class Security {
         }
         throw new RuntimeException("Không thể xác định người dùng hiện tại! Vui lòng đăng nhập lại.");
     }
-
-    public static boolean hasRole(User.RoleStatus role) {
-        User user = getCurrentUser();
-        return user != null && user.getRole() == role;
-    }
 }
