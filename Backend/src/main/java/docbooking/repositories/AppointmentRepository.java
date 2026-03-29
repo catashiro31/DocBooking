602728fda59a1d3b082be135c02652dde2628d12
Lê Văn Dũng <dunghello36dz@gmail.com>
2026-03-29 17:04:50 +0700

    fix: xóa phần thừa

--- Backend/src/main/java/docbooking/repositories/AppointmentRepository.java
+++ Backend/src/main/java/docbooking/repositories/AppointmentRepository.java
@@ -77,10 +77,6 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Intege
             "AND a.bookingStatus = 'PENDING'")
     List<Appointment> findPastDueAppointments(@Param("today") LocalDate today);
 
-    @Query("SELECT a FROM Appointment a JOIN a.schedule s " +
-            "WHERE s.dateWorking < :today " +
-            "AND a.bookingStatus = 'CONFIRMED'")
-    List<Appointment> findOverdueConfirmedAppointments(@Param("today") LocalDate today);
 
     @Query("SELECT a FROM Appointment a JOIN a.schedule s " +
             "WHERE s.doctor.user.userId = :doctorUserId " +
@@ -94,10 +90,7 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Intege
             Integer userId,
             Collection<Appointment.BookingStatus> statuses
     );
-    long countByPatient_User_UserIdAndBookingStatus(
-            Integer userId,
-            Appointment.BookingStatus status
-    );
+
 
     @Query("SELECT COUNT(a) FROM Appointment a " +
             "WHERE a.patient.user.userId = :userId " +
